fd5ba2b64468b5a5fbc07b83c47eaa0df94dfaed
Eric Marty <allopole@noreply.github.com>
2018-07-30 16:47:51 -0400

    add converter: `hcl2hex()`

--- R/converters.R
+++ R/converters.R
@@ -16,6 +16,28 @@ hex2hcl <- function(palette){
   return(p.hcl)
 }
 
+#' Convert an HCL palette to hex RGB
+#'
+#' Converts an HCL color palette supplied as a dataframe or individual arguments to an RGB palette.
+#' Returns a character vector of hex RGB colors.
+#'
+#' @param hcl Optional data frame with three columns: `$H`, `$C`, `$L`. (default = `NULL`)
+#' @param H Numeric. Hue angle (mod 360).
+#' @param C Numeric. Chroma.  Minimum value = 0.  Maximum possible depends on Hue and Lightness (about 100 to 150)
+#' @param H Numeric. Lightness (0 to 100)
+#' @return Character vector. A palette (vector of hex RGB colors).
+#' @importFrom grDevices hcl
+#' @export
+
+hcl2hex <- function(hcl=NULL,H=NULL,C=NULL,L=NULL){
+  if(!is.null(hcl)){
+    palette <- grDevices::hcl(h=hcl$H,c=hcl$C,l=hcl$L,fixup=FALSE)
+  } else {
+    palette <- grDevices::hcl(h=H,c=C,l=L,fixup=FALSE)
+  }
+  return(palette)
+}
+
 #' Simulate colorblindness
 #'
 #' From color palette (character vector of hex RGB or RGBA color codes), simulates the palette as it
